d8703969f80def4efa3153c1a1d83e5ec1309143
User <jbm.matango@yavirac.edu.ec>
2023-11-17 10:59:16 -0500

    Module 2 finished

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -13,6 +13,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 
 
 
@@ -27,6 +28,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 
     //Pipes
     HeroImagePipe,
+     ConfirmDialogComponent,
   ],
   imports: [
     CommonModule,
